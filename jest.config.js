2eccf37ea706a62c14d578b1149dab75274f3dee
Rolando Fernandez <rolando@ebi.ac.uk>
2018-10-23 11:20:01 +0100

    bug fix to let jest work with the tus-node-server

--- jest.config.js
+++ jest.config.js
@@ -70,7 +70,8 @@ module.exports = {
   moduleFileExtensions: [
     "ts",
     "tsx",
-    "js"
+    "js",
+    "json"
   ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
