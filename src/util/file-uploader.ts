bf68ff0ccb1d2f7fec5d8e09fbe290b8e9b20a14
Rolando Fernandez <rolando@ebi.ac.uk>
2018-10-30 10:20:27 +0000

    determing file sizes prior to upload to the tus server

--- src/util/file-uploader.ts
+++ src/util/file-uploader.ts
@@ -23,14 +23,36 @@ class FileUploader {
      * Given a TusUpload object, uploads the specified file
      */
     stageLocalFile(tusUpload: TusUpload) : Promise<Upload> {
+        tusUpload.fileSize = fs.statSync(tusUpload.filePath!).size;
         tusUpload.fileStream = fs.createReadStream(tusUpload.filePath!);
 
         return this.doUpload(tusUpload);
+    }
 
-        return this._getToken()
-            .then(token => {return FileUploader._insertToken(tusUpload, token)})
-            .then(tusUpload => {return FileUploader._insertSubmission(tusUpload, tusUpload.submission!)})
-            .then(tusUpload => {return this.doUpload(tusUpload)});
+    /**
+     *
+     * Uploads a file from S3, referenced in the TusUpload obj
+     *
+     * @param tusUpload
+     */
+    stageS3File(tusUpload: TusUpload) : Promise<Upload> {
+        const bucket = tusUpload.s3Bucket!;
+        const key = tusUpload.s3Key!;
+
+        return new Promise((resolve, reject) => {
+            const config = this._aws_config();
+            const s3Service = new S3(config);
+
+            s3Service.headObject({Bucket: bucket, Key: key}).promise()
+                .then((s3Obj) => {
+                    tusUpload.fileSize = s3Obj.ContentLength!.valueOf();
+                    tusUpload.fileStream = s3Service.headObject({Bucket: bucket, Key: key}).createReadStream();
+
+                    this.doUpload(tusUpload).then((upload) => {
+                        resolve(upload);
+                    })
+                });
+        })
     }
 
     /**
@@ -59,7 +81,7 @@ class FileUploader {
                 retryDelays: [0, 1000, 3000, 5000],
                 headers: tusUpload.metadataToDict(),
                 chunkSize: 50,
-                uploadSize: 2852,
+                uploadSize: tusUpload.fileSize,
                 onError: (error: any) => {
                     console.log("Failed because: " + error);
                     reject(error);
@@ -76,20 +98,6 @@ class FileUploader {
 
             upload.start();
         });
-    }
-
-    stageS3File(tusUpload: TusUpload) : Promise<Upload> {
-        const bucket = tusUpload.s3Bucket!;
-        const key = tusUpload.s3Key!;
-
-        return new Promise((resolve, reject) => {
-            const config = this._aws_config();
-            const s3Service = new S3(config);
-            tusUpload.fileStream = s3Service.getObject({Bucket: bucket, Key: key}).createReadStream();
-            this.doUpload(tusUpload).then((upload) => {
-                resolve(upload);
-            })
-        })
     }
 
    static _insertToken(tusUpload: TusUpload, token: string) : Promise<TusUpload> {
