7493bd2c5821af0ac25e65c040636238c81cbb4d
Rolando Fernandez <rolando@ebi.ac.uk>
2018-10-31 16:11:23 +0000

    *spec.ts as test files

--- jest.config.js
+++ jest.config.js
@@ -138,7 +138,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/*spec.+(ts|tsx|js)"
+    "**/*spec.+(ts|tsx)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
