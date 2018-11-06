6d0e7628436431cc8f15f5a04cb921fe55189867
Rolando Fernandez <rolando@ebi.ac.uk>
2018-11-06 13:53:55 -0500

    handling file upload messages, staging

--- src/listeners/handlers/new-file-handler.ts
+++ src/listeners/handlers/new-file-handler.ts
@@ -1,21 +1,38 @@
 import Promise from "bluebird";
 import {IHandler, AmqpMessage} from "./handler";
+import FileUploader from "../../util/file-uploader";
+import TusUpload from "../../model/tus-upload";
+import url from "url";
+
+type FileUploadMessage = {s3Url: string, fileName: string, submission: string};
 
 class NewFileHandler implements IHandler {
-    constructor() {
+    fileUploader: FileUploader;
+
+    constructor(fileUploader: FileUploader) {
+        this.fileUploader = fileUploader;
     }
 
-    handle(msg: AmqpMessage) {
-        let msgContent = null;
+    handle(msg: AmqpMessage) : Promise<void> {
         try {
-            msgContent = JSON.parse(msg.messageBytes);
+            const msgContent: FileUploadMessage = JSON.parse(msg.messageBytes);
+            const tusUploadRequest  = NewFileHandler._uploadRequestFromUploadMessage(msgContent)
+            return this.fileUploader.stageS3File(tusUploadRequest)
+                .then(() => {return Promise.resolve()});
         } catch (err) {
             console.error("Failed to parse message content (ignoring): " + msg.messageBytes);
-            return;
+            return Promise.reject(err);
         }
+    }
 
-        return msgContent;
-        // TODO: handle new File messages by uploading to the USI tus.io server
+    static _uploadRequestFromUploadMessage(uploadMessage: FileUploadMessage) : TusUpload {
+        const tusUpload = new TusUpload();
+
+        tusUpload.setS3Url(new url.URL(uploadMessage.s3Url));
+        tusUpload.submission = uploadMessage.submission;
+        tusUpload.fileName = uploadMessage.fileName;
+
+        return tusUpload;
     }
 }
 
