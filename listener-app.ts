cc6f9929de897a8e5659917b5861d9936abc2df0
Rolando Fernandez <rolando@ebi.ac.uk>
2018-11-28 16:37:16 +0000

    shelving the listener interface for now

---
+++ listener-app.ts
@@ -0,0 +1,35 @@
+const config = require('config');
+
+const TokenManager = require("src/util/token-manager");
+const NewFileListener = require("src/listeners/new-file-listener");
+const NewFileHandler = require("src/listeners/handlers/local-file-upload-handler");
+
+
+/* ----------------------------------- */
+
+const tokenManager = (() => {
+    const authUrl = config.get("AUTH.authUrl");
+    const credentials = config.get("AUTH.credentials");
+
+    return new TokenManager(authUrl, credentials);
+})();
+
+const fileUploader = (() => {
+
+})();
+
+const newFileHandler = (() => {
+    return new NewFileHandler();
+})();
+
+const newFileListener = (() => {
+    const rabbitConnectionConfig = config.get("AMQP.newFile.connection");
+    const rabbitMessagingConfig = config.get("AMQP.newFile.messaging");
+
+    const exchange = rabbitMessagingConfig.exchange;
+    const queueName = rabbitMessagingConfig.queueName;
+    const exchangeType = rabbitMessagingConfig.exchangeType;
+
+    return new NewFileListener(rabbitConnectionConfig, exchange, queueName, newFileHandler, exchangeType);
+})();
+
