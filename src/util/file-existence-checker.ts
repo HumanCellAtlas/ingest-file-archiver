fcfe6e58060032171f93690b9b736a6b37e52eca
Rolando Fernandez <rolando@ebi.ac.uk>
2018-11-30 11:58:22 +0000

    util class for checking file existence

---
+++ src/util/file-existence-checker.ts
@@ -0,0 +1,22 @@
+import Promise from "bluebird";
+import {spawn} from "child_process";
+
+class FileExistenceChecker {
+    constructor() {
+
+    }
+
+    static fileExists(filePath: string): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            const checkFileExistsProcess = spawn("ls", [filePath]);
+
+            checkFileExistsProcess.on("exit", (code: number, signal: string) => {
+                resolve(code == 0);
+            });
+
+           checkFileExistsProcess.on("error", err => {
+                reject(err);
+            });
+        });
+    }
+}
